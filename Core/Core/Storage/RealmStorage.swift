971e83879969cca89b4ecc2b2a169fd5a74265e1
Eugene Yatsenko <eyatsenko@perpet.io>
2023-03-20 18:44:23 +0200

    feat: add storage

---
+++ Core/Core/Storage/RealmStorage.swift
@@ -0,0 +1,184 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2023-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import RealmSwift
+
+final class RealmStorage: LocalStorage {
+
+    static let `default`: RealmStorage = RealmStorage()
+
+    private init() {
+        configure()
+    }
+
+    let dataBaseQueue: RealmDatabaseQueue = .init()
+    lazy var dataBaseThread: RealmDatabaseThread = RealmDatabaseThread().apply {
+        $0.workingQueue = { [unowned self] key in
+            self.dataBaseQueue.get(by: key)
+        }
+    }
+    var realm: Realm? {
+        try? dataBaseThread.getInstance()
+    }
+    private var config: Realm.Configuration = .defaultConfiguration
+
+    func addOrUpdate<T>(
+        value: T,
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    ) where T: Storable {
+        dataBaseQueue.background.async { [weak self] in
+            guard let realm = self?.realm else {
+                completionHandler(.failure(RealmError.error))
+                return
+            }
+            do {
+                try realm.write {
+                    realm.add(value)
+                    completionHandler(.success(()))
+                }
+            } catch {
+                completionHandler(.failure(error))
+            }
+        }
+    }
+
+    func addOrUpdate<T>(
+        values: [T],
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    ) where T: Storable {
+        dataBaseQueue.background.async { [weak self] in
+            guard let self = self else {
+                completionHandler(.failure(RealmError.error))
+                return
+            }
+            let group = DispatchGroup()
+            values.forEach {
+                group.enter()
+                self.addOrUpdate(
+                    value: $0
+                ) { _ in
+                    group.leave()
+                }
+            }
+            group.notify(queue: self.dataBaseQueue.background) {
+                completionHandler(.success(()))
+            }
+        }
+    }
+
+    func objects<T>(
+        _ type: T.Type,
+        completionHandler: @escaping ([T]) -> Void
+    ) where T: Storable {
+        dataBaseQueue.background.async { [weak self] in
+            guard let realm = self?.realm else {
+                completionHandler([])
+                return
+            }
+            let objects = realm.objects(type)
+            completionHandler(Array(objects))
+        }
+    }
+
+    func object<T, KeyType>(
+        _ type: T.Type,
+        forPrimaryKey key: KeyType,
+        completionHandler: @escaping (T?) -> Void
+    ) where T: Storable {
+        dataBaseQueue.background.async { [weak self] in
+            guard let realm = self?.realm else {
+                completionHandler(nil)
+                return
+            }
+
+            let object = realm.object(ofType: type, forPrimaryKey: key)
+            completionHandler(object)
+
+        }
+    }
+
+    func delete<T>(
+        value: T,
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    ) where T: Storable {
+        dataBaseQueue.background.async { [weak self] in
+            guard let realm = self?.realm else {
+                completionHandler(.failure(RealmError.error))
+                return
+            }
+            do {
+                try realm.write {
+                    realm.delete(value)
+                    completionHandler(.success(()))
+                }
+            } catch {
+                print(error.localizedDescription)
+                completionHandler(.failure(error))
+            }
+        }
+    }
+
+    func delete<T>(
+        values: [T],
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    ) where T: Storable {
+        dataBaseQueue.background.async { [weak self] in
+            guard let self = self else {
+                completionHandler(.failure(RealmError.error))
+                return
+            }
+            let group = DispatchGroup()
+            values.forEach {
+                group.enter()
+                self.delete(
+                    value: $0
+                ) { _ in
+                    group.leave()
+                }
+            }
+            group.notify(queue: self.dataBaseQueue.background) {
+                completionHandler(.success(()))
+            }
+        }
+    }
+
+    func deleteAll(completionHandler: @escaping (Result<Void, Error>) -> Void) {
+        dataBaseQueue.background.async { [weak self] in
+            guard let self = self else {
+                completionHandler(.failure(RealmError.error))
+                return
+            }
+            do {
+                try self.realm?.write {
+                    self.realm?.deleteAll()
+                }
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+    }
+
+    private func configure() {
+        let config = Realm.Configuration(
+            schemaVersion: 1,
+            migrationBlock: { _, _ in },
+            deleteRealmIfMigrationNeeded: true
+        )
+        self.config = config
+    }
+}
---
+++ Core/Core/Storage/Storable.swift
@@ -0,0 +1,142 @@
+//
+// This file is part of Canvas.
+// Copyright (C) 2023-present  Instructure, Inc.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+//
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+//
+
+import RealmSwift
+import Foundation
+
+public final class Dog: Object, Storable {
+   public convenience init(name: String, age: Int) {
+        self.init()
+        self.name = name
+        self.age = age
+       self._id = Foundation.UUID().uuidString
+    }
+
+    @Persisted(primaryKey: true) var _id: String
+    @Persisted public var name: String
+    @Persisted public var age: Int
+}
+
+public struct StorageProvider {
+    static public var current: LocalStorage = RealmStorage.default
+}
+
+public protocol Storable: Object, RealmFetchable {
+    static func all(
+        in storage: LocalStorage,
+        completionHandler: @escaping ([Self]) -> Void
+    )
+
+    static func object<KeyType>(
+        in storage: LocalStorage,
+        forPrimaryKey key: KeyType,
+        completionHandler: @escaping (_ value: Self?) -> Void
+    )
+
+    func addOrUpdate<T: Storable>(
+        in storage: LocalStorage,
+        value: T,
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    )
+
+    func delete<T: Storable>(
+        in storage: LocalStorage,
+        value: T,
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    )
+}
+
+extension Storable {
+    func store(in storage: LocalStorage) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            storage.addOrUpdate(value: self) { result in
+                continuation.resume(with: result)
+            }
+        }
+    }
+
+    func delete(from storage: LocalStorage) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            storage.delete(value: self) { result in
+                continuation.resume(with: result)
+            }
+        }
+    }
+
+    public static func all(
+        in storage: LocalStorage,
+        completionHandler: @escaping ([Self]) -> Void
+    ) {
+        storage.objects(Self.self, completionHandler: completionHandler)
+    }
+
+    public static func object<KeyType>(
+        in storage: LocalStorage,
+        forPrimaryKey key: KeyType,
+        completionHandler: @escaping (_ value: Self?) -> Void
+    ) {
+        storage.object(Self.self, forPrimaryKey: key, completionHandler: completionHandler)
+    }
+
+    public func addOrUpdate<T: Storable>(
+        in storage: LocalStorage,
+        value: T,
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    ) {
+        storage.addOrUpdate(value: value, completionHandler: completionHandler)
+    }
+
+    public func delete<T: Storable>(
+        in storage: LocalStorage,
+        value: T,
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    ) {
+        storage.delete(value: self, completionHandler: completionHandler)
+    }
+}
+
+public protocol LocalStorage {
+    func objects<T: Storable>(
+        _ type: T.Type,
+        completionHandler: @escaping ([T]) -> Void
+    )
+    func object<T: Storable, KeyType>(
+        _ type: T.Type,
+        forPrimaryKey key: KeyType,
+        completionHandler: @escaping (_ value: T?) -> Void
+    )
+    func addOrUpdate<T: Storable>(
+        value: T,
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    )
+    func addOrUpdate<T: Storable>(
+        values: [T],
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    )
+    func delete<T: Storable>(
+        value: T,
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    )
+    func delete<T: Storable>(
+        values: [T],
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    )
+    func deleteAll(
+        completionHandler: @escaping (Result<Void, Error>) -> Void
+    )
+}
